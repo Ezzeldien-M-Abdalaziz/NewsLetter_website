8df3a8ad0c2ed9071dd2377ae6856fd3e49fc441
Ezzeldien Magdy <139116899+Ezzeldien-M-Abdalaziz@users.noreply.github.com>
2025-06-15 00:06:12 +0300

    contact controller

--- app/Http/Controllers/Frontend/ContactController.php
+++ app/Http/Controllers/Frontend/ContactController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Frontend\ContactRequest;
+use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ContactController extends Controller
 {
@@ -11,7 +14,20 @@ class ContactController extends Controller
         return view('frontend.contact');
     }
 
-    public function store(Request $request){
+    public function store(ContactRequest $request){
+        $request->validated();
 
+        $request->merge([
+            'ip_address' => $request->ip()
+        ]);
+
+        $contact = Contact::create($request->except('_token()')); //token is sent by the form by default , so we need to remove it because it is not a field in the database
+
+        if(!$contact){
+            Session::flash('error', 'Something went wrong');
+            return redirect()->back();
+        }
+        Session::flash('success', 'Message sent successfully');
+        return redirect()->back();
     }
 }
