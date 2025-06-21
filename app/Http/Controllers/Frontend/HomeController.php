20b00732483c6ae75c9612d0c186d1f5056125dd
Ezzeldien Magdy <139116899+Ezzeldien-M-Abdalaziz@users.noreply.github.com>
2025-06-21 08:14:08 +0300

    fix query

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $posts = Post::active()->active()->with('images')->latest()->paginate(9);
+        $posts = Post::active()->with('images')->latest()->paginate(9);
         $greatest_posts_views = Post::active()->orderBy('num_of_views', 'desc')->take(5)->get();
         $oldest_news = Post::active()->oldest()->take(3)->get();
 
@@ -20,7 +20,7 @@ class HomeController extends Controller
             ->take(3)
             ->get();
 
-        $categories = Category::active()->all();
+        $categories = Category::active()->get();
 
         $categories_with_posts = $categories->map(function ($category) {
             $category->posts = $category->posts()->limit(4)->get();
